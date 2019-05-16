bc25dbcfe10a06508179790cef3d4098f06b2dd1
Divine C <cdivine304@gmail.com>
2019-05-16 09:48:29 +0530

    Add files via upload

---
+++ precache-manifest.2a2cd39e642bf6094dea9fa5bb121df7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5813df188d12d4d35bae50b4b2123cb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56d41afdc5f5ad695d9c",
+    "url": "/static/css/main.d5349679.chunk.css"
+  },
+  {
+    "revision": "28a3dc9126976de440dc",
+    "url": "/static/js/2.f8b1893e.chunk.js"
+  },
+  {
+    "revision": "56d41afdc5f5ad695d9c",
+    "url": "/static/js/main.570b5b83.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
